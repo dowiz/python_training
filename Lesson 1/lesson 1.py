6fbdd7c40ee732f68910ae170a868604f0db5e41
Олександр Єрьомін <eriomin.oe@gmail.com>
2024-03-09 13:32:58 +0200

    comented code

--- Lesson 1/lesson 1.py
+++ Lesson 1/lesson 1.py
@@ -1,8 +1,8 @@
 # name = input("What is yoyur name? ")
 # print(f"Hello, {name}!")
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-print(type(num1), type(num2))
-sum = num1 + num2
-print(f"Result = {sum}")
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# print(type(num1), type(num2))
+# sum = num1 + num2
+# print(f"Result = {sum}")
