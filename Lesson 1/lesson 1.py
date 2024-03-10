6c665717b496b7068bad54e3f1ea17fced20d89f
Олександр Єрьомін <eriomin.oe@gmail.com>
2024-03-10 15:10:35 +0200

    added comparison

--- Lesson 1/lesson 1.py
+++ Lesson 1/lesson 1.py
@@ -1,8 +1,15 @@
 # name = input("What is yoyur name? ")
 # print(f"Hello, {name}!")
 
-# num1 = int(input("Enter first number: "))
-# num2 = int(input("Enter second number: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 # print(type(num1), type(num2))
 # sum = num1 + num2
 # print(f"Result = {sum}")
+
+if num1 > num2:
+    print(f"{num1} > {num2}")
+elif num1 < num2:
+    print(f"{num1} < {num2}")
+else:
+    print(f"{num1} = {num2}")
