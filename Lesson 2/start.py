1cb2cbb6d5ce6a529970a63e2e7107c117fd2017
Олександр Єрьомін <eriomin.oe@gmail.com>
2024-03-16 10:36:52 +0200

    results of the first pair

---
+++ Lesson 2/start.py
@@ -0,0 +1,11 @@
+def plus(a, b):
+    return a + b
+
+
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+sum_of_numbers = plus(first_number, second_number)
+
+print(f"First number = {first_number}\nSecond number = {
+      second_number}\nSum of numbers = {sum_of_numbers}")
