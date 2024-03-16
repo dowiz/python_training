f483926f63261f3de34dc8522871406fe1dbe02b
Олександр Єрьомін <eriomin.oe@gmail.com>
2024-03-16 17:20:19 +0200

    added filling of the users dictionary

--- Lesson 2/start.py
+++ Lesson 2/start.py
@@ -1,5 +1,7 @@
 import paramiko
 
+users = {}
+
 
 def get_traffic():
     ssh_client = paramiko.SSHClient()
@@ -8,12 +10,33 @@ def get_traffic():
                        username='python', password='python')
 
     stdin, stdout, stdaddr = ssh_client.exec_command(
-        f"/interface monitor-traffic interface=ether1")
+        f"/caps-man registration-table print proplist=mac-address")
+
+    namecolumns = list()
+    columns = {}
 
     for item in stdout:
-        print(item)
+        if item.find('Columns:') != -1:
+            for column in item.strip('Columns: ').strip().split():
+                namecolumns.append(column)
+        elif item.find(namecolumns[0]) != -1:
+            i = 0
+            for column in item.strip().split():
+                columns[i] = column
+                i += 1
+        else:
+            i = 0
+            for value in item.strip().split():
+                if i == 0:
+                    i = 0
+                else:
+                    users[value] = {columns[i]: value}
+
+                i += 1
 
     ssh_client.close()
 
 
 get_traffic()
+
+print(users)
