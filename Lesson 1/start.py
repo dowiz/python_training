4e57fc84b6483f6d1545cc64c2252900161351cf
Олександр Єрьомін <eriomin.oe@gmail.com>
2024-03-16 10:16:43 +0200

    task 3

--- Lesson 1/start.py
+++ Lesson 1/start.py
@@ -1,8 +1,29 @@
 # name = input("What is yoyur name? ")
 # print(f"Hello, {name}!")
 
-# num1 = int(input("Enter first number: "))
-# num2 = int(input("Enter second number: "))
+def input_two_numbers():
+    global num1
+    global num2
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+
+def sum_of_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+
+def number_difference(num1, num2):
+    if num1 > num2:
+        result = num1 - num2
+    else:
+        result = num2 - num1
+    return result
+
+
+def product_of_numbers(num1, num2):
+    Result = num1 * num2
+    return Result
 
 # print(type(num1), type(num2))
 # sum = num1 + num2
@@ -15,6 +36,12 @@
 # else:
 #     print(f"{num1} = {num2}")
 
+
 print("\t\t\tNothing\n\t\t\twill work\n\t\t\tunless you do")
 print("")
 print("\t\"Anyone who\n\t   stops\n\t      learning is old,\n\t         whether at twenty or eighty\"\n\t\t\t\t  Henry Ford")
+print("")
+input_two_numbers()
+print("")
+print(f"Sum of numbers:\n{sum_of_numbers(num1, num2)}\nNumber difference:\n{
+      number_difference(num1, num2)}\nProduct of numbers:\n{product_of_numbers(num1, num2)}")
