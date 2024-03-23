b4e7c20de0fc8d71af770e583dbbee824f3fd6b4
Олександр Єрьомін <eriomin.oe@gmail.com>
2024-03-23 10:18:19 +0200

    Demonstrate list operations

--- Lesson 3/start.py
+++ Lesson 3/start.py
@@ -1,5 +1,31 @@
-fruits = ["apple", "banana", "cherry"]
+fruits = ["apple", "banana", "True", "cherry", "34", ["test", 100], "Demo"]
 
-print(fruits[0])
+# print("=====================================")
+# fruits[6] = "Python"
 
-print("test")
+# for item in fruits:
+#     print(item)
+
+# Last element
+print(fruits[-1])
+
+# Length of the list
+print(len(fruits))
+print("===========append=====================")
+fruits.append("TypeScript")
+print(fruits)
+print("===========insert=====================")
+fruits.insert(2, "C++")
+print(fruits)
+print("===========pop=====================")
+fruits.pop(1)
+print(fruits)
+print("===========remove=====================")
+fruits.remove(["test", 100])
+print(fruits)
+print("===========reverse=====================")
+fruits.reverse()
+print(fruits)
+print("===========sort=====================")
+fruits.sort()
+print(fruits)
